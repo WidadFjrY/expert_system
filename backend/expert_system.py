88a8bb38fb79efde223f8b2922b77106f33e7050
Widad FjrY <widadzdadz@gmail.com>
2025-03-30 14:14:47 +0700

    Add: condition, diseases, solution and rules

--- backend/expert_system.py
+++ backend/expert_system.py
@@ -2,26 +2,65 @@ from typing import List, Dict, Set
 
 class TomatoExpertSystem:
     def __init__(self):
-        self.rules = [
-            {
-                "id": "", # Id rule, contoh R01
-                "conditions": {}, # Kode kondisi, contoh {"C01", "C02", "C03"},
-                "disease": "", # Kode penyakit, contoh "D01"
-                "solutions": [], # Kode solusi, contoh ["S01", "S02", "S03"]
-            },
-        ]
-        
         self.mappings = {
             "conditions": {
-                # Variable kondisi, contoh  "C01": "Daun menguning dengan tepi coklat",
+                "C01": "Batang dan daun atas layu",
+                "C02": "Bercak coklat sampai hitam pada daun",
+                "C03": "Ada bercak kecil berwarna coklat pada buah",
+                "C04": "Ada bercak kecil berair dan membuat cekung pada buah",
+                "C05": "Pada pangkal buah dekat tangkai ada becak ungu",
+                "C06": "Daun keriting dan kering",
+                "C07": "Batang tampak kering memanjang berwarna abu-abu",
+                "C08": "Ada lubang buah setiap tomat",
+                "C09": "Batang mudah patah",
+                "C10": "Daun keriting dan kerdil, melengkung ke bawah, daun terlihat seperti terselubungi tepung putih",
+                "C11": "Daun menyempit seperti pita, mengecil dan menggulung ke atas",
+                "C12": "Buah busuk dan jika dibuka ada belatung"
             },
             "diseases": {
-                # Variable penyakit, contoh "D01": "Penyakit Hawar Daun (Early Blight)",
+                "D01": "Penyakit Layu Bakteri",
+                "D02": "Busuk Lunak Bakteri",
+                "D03": "Penyakit Cekik",
+                "D04": "Busuk Daun",
+                "D05": "Bercak Kuning",
+                "D06": "Layu Cendawan",
+                "D07": "Penyakit Ujung Keriting",
+                "D08": "TMV (Tomato Mozaik Virus)",
+                "D09": "PVX (Potato Virus X)",
+                "D10": "PVY (Potato Virus Y)",
+                "D11": "Busuk Ujung Buah",
+                "D12": "Pecah Buah"
             },
             "solutions": {
-                # Variable solusi, contoh "S01": "Gunakan fungisida mengandung chlorothalonil",
+                "S01": "Memakai benih yang resisten",
+                "S02": "Memakai mulsa plastik",
+                "S03": "Menjauhi budidaya tomat pada tempat yang terkena jamur",
+                "S04": "Memberi jeda yang lama pada lahan hingga ditanami kembali",
+                "S05": "Harus cepat dicabut dan dibakar, jangan dipendam, memutuskan siklus tanaman tomat",
+                "S06": "Memakai varietas unggul, memakai varietas bebas jamur",
+                "S07": "Penyemprotan memakai Fungisida",
+                "S08": "Memakai kayu untuk menyokong tanaman supaya buah tak menyentuh tanah",
+                "S09": "Merotasi pembibitan tanaman",
+                "S10": "Penyemprotan memakai bakterisida yang memakai antibiotik dengan dosis sesuai panduan",
+                "S11": "Membuang ulat dan telur lalu membakarnya",
+                "S12": "Menjaga kebersihan kebun dari Gulma serta semak belukar",
+                "S13": "Membuat perangkat ultraviolet",
+                "S14": "Penyemprotan memakai insektisida",
+                "S15": "Mengolah tanah dengan baik",
+                "S16": "Memakai Mulva Plastik Perak",
+                "S17": "Memakai Mulva Plastik Jerami atau Mulsa Kuning",
+                "S18": "Membalik tanah dan Membiarkan Terkena Sinar Matahari Selama Beberapa Hari"
             }
         }
+        
+        self.rules = [
+            {
+                "id": "R01", 
+                "conditions": {"C01", "C02", "C07", "C09"}, 
+                "disease": "D01", 
+                "solutions": ["S01", "S02", "S03", "S04"], 
+            },
+        ]
 
     def forward_chain(self, input_conditions: Set[str]) -> List[Dict]:
         results = []
